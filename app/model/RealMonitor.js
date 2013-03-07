aeb963175b7eae35d1ae9e292cdb9488b65c9a36
Rainbow <Rainbow@Rainbow-PC>
2013-03-08 03:15:06 +0800

    加入了导航页

--- app/model/RealMonitor.js
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-Ext.define('Duyun.model.RealMonitor', {
-    extend: 'Ext.data.Model',
-    fields: [{name: 'time', type: 'date'},//'int'},
-             {name: 'value', type: 'float'}]
-});
--- app/store/RealMonitor.js
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-Ext.define('Duyun.store.RealMonitor', {
-    extend: 'Ext.data.Store',
-    requires: 'Duyun.model.RealMonitor',
-    model: 'Duyun.model.RealMonitor'
-});
